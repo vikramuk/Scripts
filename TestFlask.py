19b3c5b09d9b8e4bad7350c1963729f2c733b647
vikramuk <vikramuk@gmail.com>
2015-08-26 14:52:00 +0530

    Create TestFlask.py

---
+++ TestFlask.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+#app.run(host='0.0.0.0')
+app.run(debug=True)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
