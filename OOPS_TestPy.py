e215870f06d1eae1bfea73f362fa5b6367e31f78
vikramuk <vikramuk@gmail.com>
2015-11-16 12:25:43 +0530

    Create OOPS_TestPy.py

---
+++ OOPS_TestPy.py
@@ -0,0 +1,20 @@
+class Test():
+    name =""
+    
+    def __init__(self, name):
+        if name != "":
+            self.name = name
+        else:
+            name = "Test"  
+
+    @staticmethod
+    def TestX():
+        print "In TestX:" + "Hi Vikram"
+        
+    def TestY(self, name):
+        print "In TestY:" + "Hi " + name       
+    
+if __name__ == "__main__":
+    c1=Test("")
+    c1.TestX()
+    c1.TestY("Vikram")
